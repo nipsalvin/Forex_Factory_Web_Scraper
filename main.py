f72c5a2dc1fc2f3d536475282230bb55838635d0
nipsalvin <amwaniki.am@gmail.com>
2024-10-11 12:31:02 +0300

    now getting `currency` and news` cells

--- main.py
+++ main.py
@@ -12,110 +12,64 @@ from time import sleep
 import requests
 
 def get_filtered_page(base_url):
-    # Set up the Selenium WebDriver
-    # import ipdb; ipdb.set_trace()
-    # import ipdb; ipdb.set_trace()
+    """
+    Set up the Selenium WebDriver and get the filtered page.
+
+    Args:
+        base_url (str): The base URL of the website.
+
+    Returns:
+        soup (BeautifulSoup): The parsed page source using BeautifulSoup.
+        filtered_folders (list): The news folders with titles in news_folder_titles.
+    """
     chrome_options = Options()
     # chrome_options.add_argument("--headless")  # This argument configures Chrome to run in headless mode.
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     driver.get(base_url)
     driver.maximize_window()
-    # Click on the filter element
-    filter_element = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.calendar__filters.right.imagefade.noborder a.highlight.filters"))
-    )
-    filter_element.click()
-    
-    # Select none on the filters
-    currency_none_btn = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[2]/p/span/a[2]'))
-    )
-    currency_none_btn.click()
-    impact_none_btn = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div/p/span/a[2]'))
-    )
-    impact_none_btn.click()
-
-    # Select the red folder
-    red_folder = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div/div/div[1]/div[2]/label/span'))
-    )
-    red_folder.click()
-
-    # Select the orange folder
-    orange_folder = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div/div/div[2]/div[2]/label/span'))
-    )
-    orange_folder.click()
-
-    # Select the grey folder
-    grey_folder = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div/div/div[4]/div[2]/label/span'))
-    )
-    grey_folder.click()
-
-    # Select USD filter
-    usd_filter = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div[5]/div[2]/label'))
-    )
-    usd_filter.click()
-
-    # Select GBP filter
-    gbp_filter = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div[2]'))
-    )
-    gbp_filter.click()
-
-    # Apply filters
-    apply_filter_btn = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/section[2]/div[3]/div/div/div/div/div[2]/div[2]/div/table/tbody/tr/td[3]/input[1]'))
-    )
-    import ipdb; ipdb.set_trace()
-    # apply_filter_btn.send_keys("\n") #Not working
-    # apply_filter_btn.click() #Not working
-
-    # Try different methods to click the button
-    try:
-        # Method 1: Regular click
-        apply_filter_btn.click()
-    except:
-        try:
-            # Method 2: JavaScript click
-            driver.execute_script("arguments[0].click();", apply_filter_btn)
-        except:
-            # Method 3: Action chains
-            ActionChains(driver).move_to_element(apply_filter_btn).click().perform()
-
-    # Trigger mousedown, mouseup, and click via JavaScript
-    driver.execute_script("""
-        var button = arguments[0];
-        var mousedownEvent = new MouseEvent('mousedown', {bubbles: true, cancelable: true, view: window});
-        var mouseupEvent = new MouseEvent('mouseup', {bubbles: true, cancelable: true, view: window});
-        var clickEvent = new MouseEvent('click', {bubbles: true, cancelable: true, view: window});
-        
-        button.dispatchEvent(mousedownEvent);
-        button.dispatchEvent(mouseupEvent);
-        button.dispatchEvent(clickEvent);
-    """, apply_filter_btn)
-
 
     sleep(5)
 
-    news_folder_titles = ['High Impact Expected', 'Medium Impact Expected', 'Non-Economic']
-
-    news_folders  = driver.find_elements(By.CSS_SELECTOR, '.icon[title]')
-
-    if news_folders != []:
-        filtered_folders = [folder for folder in news_folders if folder.get_attribute('title') in news_folder_titles]
-    
+    # Get the page source
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
+    # Close the browser
     driver.quit()
 
-    return soup
+    return soup  # Return both soup and filtered folders
+
+def scrape_page(soup):
+    """
+    Scrape the page source and return the relevant information.
+
+    Args:
+        soup (BeautifulSoup): The parsed page source using BeautifulSoup.
+
+    Returns:
+        list: A list of dictionaries where each dictionary contains the information
+            of a news event.
+
+    """
+    # Define the target currencies and impact titles
+    target_currencies = ['GBP', 'USD']
+    impact_titles = ['High Impact Expected', 'Medium Impact Expected', 'Non-Economic']
+    
+    all_impact_cells =  soup.find_all('td', class_='calendar__cell calendar__impact')
+
+    impact_cells = [impact_cell for impact_cell in all_impact_cells if impact_cell.find('span')['title'] in impact_titles]
+
+    for impact_cell in impact_cells:
+        currency_cell = impact_cell.find_previous_sibling('td', class_='calendar__cell calendar__currency')
+
+        if currency_cell:
+            currency = currency_cell.find('span').text.strip()  # Get the currency text
+            if currency in target_currencies:
+                news_cell = currency_cell.find_next_sibling('td', class_='calendar__cell calendar__event event')
+                print(currency)
+                print(news_cell)
+            # break
 
 
-if __name__== "__main__":
-    base_url="https://www.forexfactory.com/calendar?day=today"
+if __name__ == "__main__":
+    base_url = "https://www.forexfactory.com/calendar?day=today"
     soup = get_filtered_page(base_url)
-
